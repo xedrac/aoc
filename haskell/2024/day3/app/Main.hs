900341e1342704c55e3c09e1c2e6afaf109445b8
Shawn Badger <shawnbadger@gmail.com>
2024-12-05 22:45:54 -0700

    AOC day3 part 1

--- haskell/2024/day3/app/Main.hs
+++ haskell/2024/day3/app/Main.hs
@@ -1,17 +1,27 @@
 module Main where
 
---import Text.Regex.Compat
-import Text.Regex.Posix ((=~), AllMatches (..))
-
-findMatches :: String -> String -> [String]
-findMatches input re = getAllMatches' (input =~ re :: AllMatches [] String)
-
-getAllMatches' :: AllMatches [] String -> [String]
-getAllMatches' (AllMatches matches) = matches
+import Text.Regex.TDFA
+import qualified Data.Text as T
 
 main :: IO ()
 main = do
     contents <- readFile "input"
-    let re = "mul([0-9]+,[0-9]+)"
-    let matches = findMatches contents re
-    mapM_ putStrLn matches
+    let re = "mul\\(([0-9]+),([0-9]+)\\)"
+    let matches = getAllTextMatches (contents =~ re) :: [String]
+    let numPairs = fmap parseMul matches
+    let values = fmap kaboom numPairs
+    --putStrLn $ show values
+    putStrLn $ show (fmap sum $ sequence values)
+    where
+        -- I shouldn't have to operate on Maybe's here...
+        kaboom :: Maybe (Int, Int) -> Maybe Int
+        kaboom (Just (a,b)) = Just (a * b)
+        kaboom Nothing = Nothing
+
+parseMul :: String -> Maybe (Int,Int)
+parseMul x = do
+    let trimmed = takeWhile (/= ')') (drop 4 x)
+    let tokens = T.splitOn (T.pack ",") (T.pack trimmed)
+    case tokens of
+        [s0,s1] -> Just (read (T.unpack s0), read (T.unpack s1))
+        _ -> Nothing
